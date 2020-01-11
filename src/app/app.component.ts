4e9c4825975febfc37ca503e92e9d645e9e3f665
Yuri <yuridacostagouveia@gmail.com>
2020-01-10 21:43:02 -0300

    Development: Navbar styling development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import noUiSlider from 'nouislider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  isCollapsed = true;
-  focus;
-  focus1;
-  focus2;
-  date = new Date();
-  pagination = 3;
-  pagination1 = 1;
-  constructor() {}
-
-  ngOnInit() {
-    const body = document.getElementsByTagName('body')[0];
-    body.classList.add('index-page');
-
-    const slider = document.getElementById('sliderRegular');
-
-    noUiSlider.create(slider, {
-      start: 40,
-      connect: false,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-
-    const slider2 = document.getElementById('sliderDouble');
-
-    noUiSlider.create(slider2, {
-      start: [20, 60],
-      connect: true,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-  }
-  ngOnDestroy() {
-    const body = document.getElementsByTagName('body')[0];
-    body.classList.remove('index-page');
-  }
-
-  scrollToElement(element): void {
-    element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
-  }
-
-  inProgressAnimation() {
-    const inProgress = '';
-  }
+export class AppComponent {
 }
